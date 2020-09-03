9d7789489fbfa341a7d64cb2602b3233e45eef73
Tim Jarrett <tjarrett@veracode.com>
2020-09-03 13:03:28 -0400

    prepare for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'veracode-api-py',         
+  packages = ['veracode-api-py'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'Python helper library for working with the Veracode APIs. Handles retries, pagination, and other features of the modern Veracode REST APIs.',   
+  author = 'Tim Jarrett',                  
+  author_email = 'tjarrett@veracode.com',      
+  url = 'https://github.com/tjarrettveracode',   
+  download_url = 'https://github.com/tjarrettveracode/veracode-api-py/archive/v_01.tar.gz',    
+  keywords = ['veracode', 'veracode-api'],   
+  install_requires=[            
+          'veracode-api-signing'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3'
+  ],
